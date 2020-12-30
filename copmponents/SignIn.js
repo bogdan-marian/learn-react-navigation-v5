ff1fde0d1dc00ad952dc7b7d616fa84f07b87fa6
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-30 11:59:37 +0100

    added ScreenContainer

--- copmponents/SignIn.js
+++ copmponents/SignIn.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
+import {ScreenContainer} from "./ScreenContainer"
 
 export const SignIn = ({ navigation }) => {
   return (
-    <View>
+    <ScreenContainer>
       <Text>Sign in Screen</Text>
       <Button title="Sign In" onPress={() => alert("todo!")} />
       <Button title="Create Account" onPress={() => alert("todo!")} />
-    </View>
+    </ScreenContainer>
   );
 };
