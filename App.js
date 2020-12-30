9809f3eea67e8ea4ce745e3ac5309ab4ea6dd3cb
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-30 15:41:38 +0100

    added tab navigator

--- App.js
+++ App.js
@@ -4,16 +4,28 @@ import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
 import {SignIn} from './copmponents/SignIn'
 import {CreateAccount} from './copmponents/CreateAccount'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { Home } from "./copmponents/Home";
+import {Profile} from "./copmponents/Profile"
 
 const AuthStack = createStackNavigator();
+
+const Tabs = createBottomTabNavigator();
+
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AuthStack.Navigator>
+
+      <Tabs.Navigator>
+        <Tabs.Screen name="Home" component={Home}/>
+        <Tabs.Screen name="Profile" component={Profile} />
+      </Tabs.Navigator>
+
+      {/* <AuthStack.Navigator>
         <AuthStack.Screen name="SignIn" component={SignIn} options={{title:'Sign In'}}/>
         <AuthStack.Screen name="CreateAccount" component={CreateAccount} options={{title:'Create Account'}}/>
-      </AuthStack.Navigator>
+      </AuthStack.Navigator> */}
     </NavigationContainer>
   );
 }
---
+++ copmponents/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+import {ScreenContainer} from "./ScreenContainer"
+
+export const Profile = ({ navigation }) => {
+  return (
+    <ScreenContainer>
+      <Text>Profile Screen</Text>
+      <Button title="Drawer" onPress={() => alert("todo")} />
+      <Button title="Sign Out" onPress={() => alert("todo")} />
+    </ScreenContainer>
+  );
+};
