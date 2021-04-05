e3719c3cb1d27f08b005150dfc8786468354b5af
Bogdan <bogdan.oloeriu@gmail.com>
2021-04-05 10:43:07 +0200

    added homestack screen

--- App.js
+++ App.js
@@ -1,30 +1,54 @@
 import React from "react";
-import {View, Text} from 'react-native'
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
-import {SignIn} from './copmponents/SignIn'
-import {CreateAccount} from './copmponents/CreateAccount'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { SignIn } from "./copmponents/SignIn";
+import { CreateAccount } from "./copmponents/CreateAccount";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from "./copmponents/Home";
-import {Profile} from "./copmponents/Profile"
+import { Profile } from "./copmponents/Profile";
 
 const AuthStack = createStackNavigator();
 
 const Tabs = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
 
+const HomeStackScreen = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={Home} />
+    </HomeStack.Navigator>
+  );
+};
+
+const ProfileStackScreen = () => {
+  return (
+    <ProfileStack.Navigator>
+      <ProfileStack.Screen name="Profile" component={Profile} />
+    </ProfileStack.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-
       <Tabs.Navigator>
-        <Tabs.Screen name="Home" component={Home}/>
-        <Tabs.Screen name="Profile" component={Profile} />
+        <Tabs.Screen name="Home" component={HomeStackScreen} />
+        <Tabs.Screen name="Profile" component={ProfileStackScreen} />
       </Tabs.Navigator>
 
       {/* <AuthStack.Navigator>
-        <AuthStack.Screen name="SignIn" component={SignIn} options={{title:'Sign In'}}/>
-        <AuthStack.Screen name="CreateAccount" component={CreateAccount} options={{title:'Create Account'}}/>
+        <AuthStack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{ title: "Sign In" }}
+        />
+        <AuthStack.Screen
+          name="CreateAccount"
+          component={CreateAccount}
+          options={{ title: "Create Account" }}
+        />
       </AuthStack.Navigator> */}
     </NavigationContainer>
   );
